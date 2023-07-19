cc9ddd6b92b7b4f8dd071b5f458de680349af525
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-07-19 22:02:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 function threeSum(arr, target) {
-let sum=0;
-	let i=0;
-	while(sum<=target){
-		sum=sum+arr[i];
-		i++;
+// write your code here
+	arr.sort(function(a,b){return (a-b)})
+	let j=0;
+	let k=0;
+	for(let i=0;i<arr.length;i++){
+		let j=i+1;
+		let k=arr.length-1;
+		let sum=0;
+		let ans=0;
+		while(i<j){
+			sum=arr[i]+arr[j]+arr[k];
+			let diff=sum-target;
+			if(sum<target){diff=target-sum;}
+			ans=Math.max(ans,diff);
+			if(sum==target){
+				return 0;
+			}
+			else if(sum<target){j++;}
+			else{k--;}
+		}
 	}
-	return sum;
+  return ans;
 }
 
-module.exports = threeSuaz vaqv faz Xm;
+module.exports = threeSum;
